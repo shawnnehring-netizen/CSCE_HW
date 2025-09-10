bffd825a3c04736f59ebd51eaba2330c707d29e4
Shawn Nehring <shawnnehring@gmail.com>
2025-09-09 23:08:28 -0500

    starting

--- Hw2/functions.cpp
+++ Hw2/functions.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "functions.h"
 
-// returns the boolean value true if and only if 1,000 <= a <= b < 1,000,000,000,000
+// returns the boolean value true if and only if 1,000 <= a <= b < 1000000000000
 bool is_valid_range(long long a, long long b) {
-	// TODO(student): return true if and only if 1,000 <= a <= b < 1,000,000,000,000
-	if (1000 <= a && a <= 1000000000000 && 1000 <= b && b <= 1000000000000){
-		return true;
+	// TODO(student): return true if and only if 1,000 <= a <= b < 1000000000000
+	if (1000 <= a && a < 1000000000000  && 1000 <= b && b < 1000000000000){
+		return true;     
 	}
 	else{
 		return false;	
@@ -16,44 +16,48 @@ bool is_valid_range(long long a, long long b) {
 // returns the 'p' if number is a plateau, 'b' if number is a basin, and 'n' if number is neither
 char classify_geo_type(long long number) {
 	// TODO(student): Initialize local variables
-	int num_plat = number;
-	int num_bas = number;
 	int old_rem = number % 10;
 	int step_plat = 0;
 	int step_bas = 0;
 	char value = 'n';
-	// TODO(student): Determine whether num_plat is a plateau: this is the key loop
-	while (num_plat != 0){
-		num_plat = num_plat / 10;
-		if (num_plat == 0){
+	// TODO(student): Determine whether number is a plateau: this is the key loop
+	while (number != 0 && step_plat == 2){
+		number = number / 10;
+		if (number == 0){
+			std::cout << 'p';
 			value = 'p';
 		}
-		int rem = num_plat % 10;
+		int rem = number % 10;
+		//std::cout << old_rem << ' ' << rem << ' ';
 		if (rem > old_rem && (step_plat == 0 || step_plat == 1)){
 			step_plat = 1;
 			old_rem = rem;
+			//std::cout << "increase" << std::endl;
 			continue;}
 		else if (rem == old_rem && (step_plat == 1 || step_plat == 2)){
 			step_plat = 2;
 			old_rem = rem;
+			//std::cout << "stay" << std::endl;
 			continue;}
 		else if (rem < old_rem && (step_plat == 2 || step_plat == 3)){
 			step_plat = 3;
 			old_rem = rem;
+			//std::cout << "decrease" << std::endl;
 			continue;}
 		else{
+			//std::cout << "done";
 			break;}
 
 	}
 
 	// TODO(student): Determine whether number is a basin
 	//                If you get the logic for plateau right, minor tweaks can create the code for basins.
-	while (num_bas != 0){
-		num_bas = num_bas / 10;
-		if (num_bas == 0){
+	while (number != 0){
+		number = number / 10;
+		if (number == 0 && step_bas == 2){
 			value = 'b';
 		}
-		int rem = num_bas % 10;
+		int rem = number % 10;
 		//std::cout << old_rem << ' ' << rem << ' ';
 		if (rem < old_rem && (step_bas == 0 || step_bas == 1)){
 			step_bas = 1;
@@ -86,7 +90,7 @@ plateaus_and_basins count_pb_numbers(long long a, long long b) {
 	int number_of_basins = 0;
 
 	// TODO(student): count plateaus and basins in the range [a,b]
-	int i;
+	long long i;
 	char value;
 	for (i=a;i<=b;i++){
 		//std::cout << i;
@@ -97,6 +101,7 @@ plateaus_and_basins count_pb_numbers(long long a, long long b) {
 		else if (value == 'b'){
 			number_of_basins += 1;
 		}
+		break;
 	}
 	return {number_of_plateaus, number_of_basins};
 }
