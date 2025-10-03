c9c277d801748f7314b3825ed4c9493fff537bde
Shawn Nehring <shawnnehring@gmail.com>
2025-10-03 01:55:04 -0500

    bang?

--- Hw4/functions.cpp
+++ Hw4/functions.cpp
@@ -78,7 +78,7 @@ int64_t product(int64_t a, int64_t b) {
   int64_t num_min = std::numeric_limits<int64_t>::min();
   int64_t mult = a * b;
   //std::cout << num_max << " " << num_min;
-  if ((a) < (num_min/b) || (a) > (num_max/b)){
+  if ((a) > (num_min/b) || (a) < (num_max/b)){
     throw std::overflow_error("overflow");}
   return mult;
 }
