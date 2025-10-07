dacfea758605535eb9b6ceb7643a2772dc6093ff
Shawn Nehring <shawnnehring@gmail.com>
2025-10-07 02:04:03 -0500

    as

--- Hw5/functions.cpp
+++ Hw5/functions.cpp
@@ -5,10 +5,71 @@
 #include <string>
 #include "functions.h"
 #include <vector>
-Image load_image(const std::string&) {
+Image load_image(const std::string& file) {
     // TODO(student): implement image loading
-
-    return {};
+    if (file == ""){
+        throw std::runtime_error("Invalid filename");
+    }
+    std::ifstream word_file(file);
+    if (word_file.eof()){
+        throw std::runtime_error("Invalid filename");
+    }
+    if (!(word_file.is_open())){
+        throw std::runtime_error("Failed to open <filename>");
+    }
+    std::string p3 = "";
+    long unsigned int width = 0;
+    long unsigned int height = 0;
+    int max_color = 0;
+    int red = 0;
+    int blue = 0;
+    int green = 0;
+    std::vector<Pixel> new_image = {};
+    Image newer_image = {};
+    Pixel new_pixel = {};
+    word_file >> p3;
+    word_file >> width >> height;
+    word_file >> max_color;
+    if (!(p3 == "p3" || p3 == "P3")){
+        throw std::runtime_error("Invalid type <type>");
+    }
+    if (width > MAX_WIDTH || height > MAX_HEIGHT){
+        throw std::runtime_error("Invalid dimensions");
+    }
+    if (max_color < 1 || max_color > 255){
+        throw std::runtime_error("Invalid max color");
+    }
+    long unsigned int step = 0;
+    while (word_file >> red >> green >> blue){
+        if (red < 0 || green < 0 || blue < 0 || red > max_color || green > max_color || blue > max_color){
+            throw std::runtime_error("Invalid color value");
+        }
+        new_pixel.red = red;
+        new_pixel.green = green;
+        new_pixel.blue = blue;
+        new_image.push_back(new_pixel);
+        if (step == width){
+            newer_image.push_back(new_image);
+            step = 0;
+            new_image.clear();
+        }
+        step+=1;
+    }
+    if (newer_image.size() < height){
+        throw std::runtime_error("Not enough values");
+    }
+    if (newer_image.size() > height || !(new_image.empty())){
+        throw std::runtime_error("Too many values");
+    }
+    std::vector<Pixel> final_sub = {};
+    Image final_image = {};
+    for (long unsigned int i = 0; i < width; i++){
+        for (long unsigned int j = 0; j < height; j++){
+            final_sub.push_back(newer_image[j][i]);
+        }
+        final_image.push_back(final_sub);
+    }
+    return {final_image};
 }
 
 void output_image(const std::string& file,
@@ -79,6 +140,7 @@ Pixel bicubic_interpolation(const Image& pict,
 Image scale_image(const Image& pict,
                   size_t width,
                   size_t height) {
+    Image new_image = {};
     int x_cord = 0;
     int y_cord = 0;
     Pixel pix;
@@ -93,8 +155,9 @@ Image scale_image(const Image& pict,
             x_cord = map_coordinates(pict[i].size(), width, j);
             y_cord = map_coordinates(pict.size(), height, i);
             pix = bicubic_interpolation(pict, x_cord, y_cord);
+            new_image[i][j] = pix;
         }
     }
     // TODO(student): add loops to calculate scaled images
-    return {};
+    return {new_image};
 }
