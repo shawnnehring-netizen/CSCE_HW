f5f9b05f7469d6a2649ba8a00d1c5b5297fe46fa
Shawn Nehring <shawnnehring@gmail.com>
2025-10-12 02:23:30 -0500

    first try

--- Hw6/functions.cpp
+++ Hw6/functions.cpp
@@ -3,12 +3,33 @@
 #include <vector>
 #include "functions.h"
 
-double error_calculation(const Image&,
-                         const Corner&,
-                         const Image&,
-                         const Corner&) {
+//cd Hw6
+//ls functions.cpp functions.h image_stitching.cpp provided_functions.cpp
+//g++ -std=c++17 -Wall -Wextra -Weffc++ -pedantic-errors -g *.cpp
+double error_calculation(const Image& image_1,
+                         const Corner& point_1, 
+                         const Image& image_2,
+                         const Corner& point_2) {
+    std::vector<Pixel> first_numbers(ERROR_NEIGHBORHOOD_SIZE, ERROR_NEIGHBORHOOD_SIZE);
+    std::vector<Pixel> second_numbers(ERROR_NEIGHBORHOOD_SIZE, ERROR_NEIGHBORHOOD_SIZE);
+    for (int i = 0; i < ERROR_NEIGHBORHOOD_SIZE, i++;){
+        for (int j = 0; j < ERROR_NEIGHBORHOOD_SIZE, j++;);{
+            first_numbers.push_back(image_1[point_1.x-(ERROR_NEIGHBORHOOD_SIZE/2)+i][point_1.y-(ERROR_NEIGHBORHOOD_SIZE/2)+j]);
+            second_numbers.push_back(image_1[point_1.x-(ERROR_NEIGHBORHOOD_SIZE/2)+i][point_1.y-(ERROR_NEIGHBORHOOD_SIZE/2)+j]);
+        }
+    }
+    int red_num = 0;
+    int green_num = 0;
+    int blue_num = 0;
+    double sum = 0;
+    for (long unsigneed int i = 0; i < first_numbers.size(); i++){
+        red_num = (first_numbers[i].red-second_numbers[i].red)**2;
+        green_num = (first_numbers[i].green-second_numbers[i].green)**2;
+        blue_num = (first_numbers[i].blue-second_numbers[i].blue)**2;
+        sum += (red_num+green_num+blue_num)**(1/2)
+    }
     // TODO(student): Complete error calculation function.
-    return 0;
+    return sum;
 }
 
 std::vector<CornerPair> match_corners(const Image&,
--- Hw6/functions_part1.cpp
+++ Hw6/functions_part1.cpp
@@ -1,6 +1,181 @@
 // for local testing only. do not submit.
 
+
 // TODO(student): copy to this file your implementations of
 //                load_image
 //                output_image
 //                bicubic_interpolation
+Image load_image(const std::string& file) {
+    // TODO(student): implement image loading
+    if (file == ""){
+        throw std::runtime_error("Invalid filename");
+    }
+    std::ifstream word_file(file);
+    std::ifstream too_much(file);
+    if (!(word_file.is_open())){
+        throw std::runtime_error("Failed to open "+ file);
+    }
+    if (word_file.peek()==EOF){
+        throw std::runtime_error("Failed to read type");
+    }
+    std::string p3 = "";
+    long unsigned int width = 0;
+    long unsigned int height = 0;
+    int max_color = 0;
+    int color = 0;
+    std::vector<Pixel> new_image = {};
+    Image newer_image = {};
+    Pixel new_pixel = {};
+    word_file >> p3;
+    if (!(p3 == "p3" || p3 == "P3")){
+        throw std::runtime_error("Invalid type " + p3);
+    }
+    word_file >> width >> height;
+    if (width > MAX_WIDTH || height > MAX_HEIGHT || word_file.fail() || width == 0 || height == 0){
+        throw std::runtime_error("Invalid dimensions");
+    }
+    word_file >> max_color;
+    if (max_color < 1 || max_color > 255){
+        throw std::runtime_error("Invalid max color");
+    }
+    //std::cout << max_color << " " << width << " " << height << " " << p3 << "\n";
+    long unsigned int step = 0;
+    long unsigned int color_count = 0;
+    long unsigned int count = 0;
+    while (word_file >> color){
+        if (color < 0 || color > max_color){
+            throw std::runtime_error("Invalid color value");
+        }
+        if (color_count == 0){
+            new_pixel.red = color;
+            color_count +=1;
+        }
+        else if (color_count == 1){
+            new_pixel.green = color;
+            color_count +=1 ;
+        }
+        else if (color_count == 2){
+            new_pixel.blue = color;
+            step+=1;
+            new_image.push_back(new_pixel);
+            color_count = 0;
+        }
+        //std::cout << new_pixel.red << " " << new_pixel.green << " " << new_pixel.blue << "\n";
+        if (step == width){
+            newer_image.push_back(new_image);
+            step = 0;
+            new_image.clear();
+        }
+        count+=1;
+    }
+    if (count < height*width*3){
+        throw std::runtime_error("Not enough values");
+    }
+    if (count > height*width*3){
+        throw std::runtime_error("Too many values");
+    }
+    Image final_image(width, std::vector<Pixel>(height));
+    for (long unsigned int i = 0; i < width; i++){
+        for (long unsigned int j = 0; j < height; j++){
+            final_image[i][j] = newer_image[j][i];
+        }
+    }
+    return {final_image};
+}
+
+void output_image(const std::string& file,
+                  const Image& pict) {
+    std::string line = ""; 
+    if (file == ""){
+        throw std::invalid_argument("Invalid filename");
+    }
+    std::ofstream word_file(file);
+    if (pict.size() == 0 || pict[0].size() == 0){
+        throw std::invalid_argument("Invalid image");
+    }
+    if (!(word_file.is_open())){
+        throw std::invalid_argument("Failed to open "+ file);
+    }
+    int width = pict.size();
+    int height = pict[0].size();
+    Image final_one(width, std::vector<Pixel>(height));
+    word_file << "P3\n" << width << " " << height << "\n" << "255" << "\n";
+    for (int i = 0; i < height; i++){ // Possible problem in not switching rows and columns 
+        for (int j = 0; j < width; j++){ //SHOULD BE TRANSPOSED NOW
+            word_file << (int)pict[j][i].red << " " << (int)pict[j][i].green << " " << (int)pict[j][i].blue << "\n";
+        }
+    }    // TODO(student): implement writing image to file
+}
+Pixel bicubic_interpolation(const Image& pict,
+                            double x_old,
+                            double y_old) {
+    int x = std::trunc(x_old);
+    int y = std::trunc(y_old);
+    if (pict.size() == 0 || pict[0].size() == 0){
+        throw std::invalid_argument("Invalid image");
+    }
+    int width = pict.size();
+    int height = pict[0].size();
+    if (x >= width || x < 0 || y >= height || y < 0){
+        throw std::invalid_argument("Invalid coordinate");
+    }
+    // TODO(student): Implement bicubic interpolation
+    int x_0 = 0;
+    int x_1 = x;
+    int x_2 = 0;
+    int x_3 = 0;
+    if (x < 1){
+        x_0 = x;
+    }
+    else {
+        x_0 = x - 1;
+    }
+    if (x+1 >= width){
+        x_2 = x;
+        x_3 = x;}
+    else if (x + 2 >= width){
+        x_3 = x + 1;
+        x_2 = x + 1;
+    }
+    else {
+        x_2 = x + 1;
+        x_3 = x + 2;
+    }
+
+    int y_0 = 0;
+    int y_1 = y;
+    int y_2 = 0;
+    int y_3 = 0;
+    if (y < 1){
+        y_0 = y;
+    }
+    else {
+        y_0 = y - 1;
+    }
+    if (y+1 >= height){
+        y_2 = y;
+        y_3 = y;}
+    else if (y + 2 >= height){
+        y_3 = y + 1;
+        y_2 = y + 1;
+    }
+    else {
+        y_2 = y + 1;
+        y_3 = y + 2;
+    }
+    std::vector<Pixel> x_pixels = {};
+    std::vector<int> y_values = {y_0, y_1, y_2, y_3};
+    std::vector<int> x_values = {x_0, x_1, x_2, x_3};
+    double rem_x = x_old - x;
+    double rem_y = y_old - y;
+    for (int j = 0; j < 4; j++){
+        Pixel count1x = pict[x_0][y_values[j]];
+        Pixel count2x = pict[x_1][y_values[j]];
+        Pixel count3x = pict[x_2][y_values[j]];
+        Pixel count4x = pict[x_3][y_values[j]];
+        Pixel x1 = bicubic_pixel(rem_x, count1x, count2x, count3x, count4x);
+        x_pixels.push_back(x1);
+    }
+    Pixel color = bicubic_pixel(rem_y, x_pixels[0], x_pixels[1], x_pixels[2], x_pixels[3]);
+    return {color};
+}
