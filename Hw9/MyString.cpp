024137a1e4d09c8f68a90b42afeeac7341449c6c
Shawn Nehring <shawnnehring@gmail.com>
2025-11-14 14:37:25 -0600

    FIRST

--- Hw9/MyString.cpp
+++ Hw9/MyString.cpp
@@ -1,2 +1,127 @@
 // TODO(student): Implement this source file
 #include "MyString.h"
+#include <iostream>
+#include <stdexcept>
+
+
+MyString::MyString() : str_arr{new char[1]}, size_{0}, capacity_{1}{}
+
+MyString::MyString(const MyString& str) : str_arr{new char[str.capacity_]}, size_{str.size_}, capacity_{str.capacity_} {
+    for (unsigned int i = 0; i < str.size_; i++){
+        str_arr[i] = str.str_arr[i];
+    }
+}
+
+MyString::MyString (const char* c) : str_arr{new char[1]}, size_{0}, capacity_{1}{
+    str_arr[0] = *c;
+}
+
+MyString& MyString::operator=(MyString const& new_str){
+    if (this != &new_str){
+        char* copy = new char[new_str.capacity_];
+        for(unsigned int i = 0; i < new_str.capacity_; i++){
+            copy[i] = new_str.str_arr[i];
+        }
+        delete[] str_arr;
+        str_arr = copy;
+        capacity_ = new_str.capacity_;
+        size_ = new_str.size_;
+    }
+    return *this;
+
+}
+
+int MyString::size(){
+    return size_;
+}
+
+int MyString::length(MyString str){
+    return str.size_;
+}
+
+int MyString::capacity(){
+    return capacity_;
+}
+
+void MyString::clear(){
+    size_ = 0;
+}
+
+void MyString::resize(unsigned n){
+    if(n > capacity_){
+        capacity_ *= 2;
+    }
+    size_ = n;
+}
+
+bool MyString::empty(){
+    if(str_arr == '\0'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const char& MyString::at(unsigned pos) {
+    return str_arr[pos];
+}
+
+const char& MyString::front() {
+    return str_arr[0];
+}
+
+MyString& MyString::operator+=(MyString new_str){
+    if (size_ + new_str.size_ > capacity_){
+        capacity_ *= 2;
+    }
+    for (unsigned int i = 0; i < new_str.size_; i++){
+        str_arr[i+size_] = new_str.str_arr[i];
+    }
+    return *this;
+}
+
+char* MyString::data(){
+    return str_arr;
+}
+
+bool MyString::find(MyString str){
+    for (unsigned i = 0;  i < size_; i++){
+        if (str_arr[i] == *str.str_arr){
+            return i;
+        }
+    }
+}
+
+//std::ostream& operator<<(std::ostream& os, MyString& new_str){
+//    for (unsigned int i = 0; i < new_str.size(); i++){
+//       os << new_str.data()[i];
+//    }
+//    return os;
+//}
+
+MyString& operator+(MyString old, MyString new_){
+    if (old.size() + new_.size() > old.capacity()){
+        old.resize(old.size() + new_.size());
+    }
+    for (unsigned int i = 0; i < new_.size(); i++){
+        old.data()[i+old.size()] = new_.data()[i];
+    }
+    return old;
+}
+
+bool operator==(MyString old, MyString new_){
+    if (old.size() == new_.size()){
+        for (unsigned int i = 0; i < old.size(); i++){
+            if (new_.data()[i] != old.data()[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+MyString::~MyString(){
+    delete[] str_arr;
+}
--- Hw9/MyString.h
+++ Hw9/MyString.h
@@ -1,30 +1,31 @@
 // TODO(student): Implement this header file
 # ifndef _MYSTRING_H
 # define _MYSTRING_H
+#include <iostream>
+#include <stdexcept>
 
 class MyString{
     private:
         char* str_arr;
+        unsigned size_;
+        unsigned capacity_;
+        //int npos;
     public:
         MyString();
         MyString(const MyString& str);
         MyString (const char* c);
-        MyString operator=(MyString prev_str, MyString new_str);
-        int size(MyString str);
+        int size();
         int length(MyString str);
-        int capacity(MyString str);
-        void clear(MyString str);
-        void resize(size_t n);
-        void empty();
-        const char& at (size_t pos) const;
-        const char& front() const;
-        MyString operator+=(MyString pre_str, MyString new_str);
-        char data(MyString str);
+        int capacity();
+        MyString& operator=(MyString const& new_str);
+        void clear();
+        void resize(unsigned n);
+        bool empty();
+        MyString& MyString::operator+=(MyString new_str);
+        const char& at (unsigned pos);
+        const char& front();
+        char* data();
         bool find(MyString str);
-        npos = ;
-        MyString operator<<(MyString pre_str, MyString new_str);
-        MyString operator+(MyString pre_str, MyString new_str);
-        MyString operator==(MyString pre_str, MyString new_str);
         ~MyString();
 };
 # endif
