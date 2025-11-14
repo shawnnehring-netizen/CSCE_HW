2a34f827c771ad45f8446ddb06ced71c63be315a
Shawn Nehring <shawnnehring@gmail.com>
2025-11-14 17:45:16 -0600

    hope

--- Hw9/MyString.cpp
+++ Hw9/MyString.cpp
@@ -7,19 +7,29 @@
 MyString::MyString() : str_arr{new char[1]}, size_{0}, capacity_{1}{}
 
 MyString::MyString(const MyString& str) : str_arr{new char[str.capacity_]}, size_{str.size_}, capacity_{str.capacity_} {
-    for (unsigned int i = 0; i < str.size_; i++){
+    for (size_t i = 0; i < str.size_; i++){
         str_arr[i] = str.str_arr[i];
     }
 }
 
-MyString::MyString (const char* c) : str_arr{new char[1]}, size_{0}, capacity_{1}{
-    str_arr[0] = *c;
+MyString::MyString (const char* c){
+    size_t i = 0;
+    while(c[i] != '\0'){
+        i++;
+    }
+    size_ = i;
+    capacity_ = i+1;
+    str_arr = new char[capacity_];
+    for (size_t j = 0; j < size_;j++){
+        str_arr[j] = c[j];
+    }
+    str_arr[size_] = '\0';
 }
 
 MyString& MyString::operator=(MyString const& new_str){
     if (this != &new_str){
         char* copy = new char[new_str.capacity_];
-        for(unsigned int i = 0; i < new_str.capacity_; i++){
+        for(size_t i = 0; i < new_str.capacity_; i++){
             copy[i] = new_str.str_arr[i];
         }
         delete[] str_arr;
@@ -67,7 +77,7 @@ MyString& MyString::operator+=(MyString new_str){
     if (size_ + new_str.size_ > capacity_){
         capacity_ *= 2;
     }
-    for (unsigned int i = 0; i < new_str.size_; i++){
+    for (size_t i = 0; i < new_str.size_; i++){
         str_arr[i+size_] = new_str.str_arr[i];
     }
     return *this;
@@ -108,7 +118,7 @@ MyString operator+(MyString old, MyString new_){
     if (old.size() + new_.size() > old.capacity()){
         old.resize(old.size() + new_.size());
     }
-    for (unsigned int i = 0; i < new_.size(); i++){
+    for (size_t i = 0; i < new_.size(); i++){
         old.data()[i+old.size()] = new_.data()[i];
     }
     return old;
@@ -116,7 +126,7 @@ MyString operator+(MyString old, MyString new_){
 
 bool operator==(MyString old, MyString new_){
     if (old.size() == new_.size()){
-        for (unsigned int i = 0; i < old.size(); i++){
+        for (size_t i = 0; i < old.size(); i++){
             if (new_.data()[i] != old.data()[i]){
                 return false;
             }
--- Hw9/MyString.h
+++ Hw9/MyString.h
@@ -7,22 +7,22 @@
 class MyString{
     private:
         char* str_arr;
-        unsigned size_;
-        unsigned capacity_;
-        const unsigned npos = -1;
+        size_t size_;
+        size_t capacity_;
     public:
+        const size_t npos = -1;
         MyString();
         MyString(const MyString& str);
         MyString (const char* c);
         MyString& operator=(MyString const& new_str);
-        unsigned size()const;
-        unsigned length()const;
-        unsigned capacity()const;
+        size_t size()const;
+        size_t length()const;
+        size_t capacity()const;
         void clear();
-        void resize(unsigned n);
+        void resize(size_t n);
         bool empty();
         MyString& operator+=(MyString new_str);
-        const char& at (unsigned pos)const;
+        const char& at (size_t pos)const;
         const char& front()const;
         char* data() const;
         bool find(MyString str);
